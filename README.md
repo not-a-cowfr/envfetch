53673d030db0f770e71f8a86b6b86b178ac92751
ANKDDEV <andrekabatareika@gmail.com>
2024-11-30 17:01:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,145 @@
+<h1 align="center">envfetch</h1>
+<h5 align="center">Lightweight cross-platform CLI tool for working with environment variables</h5>
+
+# Features
+- [x] Print all environment variables
+- [x] Get value of variable by name
+    - [x] Show similar variables if variable not found
+- [x] Set variable
+- [x] Delete variable
+- [x] Load variables from dotenv-style file
+- [ ] Globally set variables
+- [ ] Globally delete variables
+- [ ] Globally load variables from dotenv-style file
+- [ ] Set and delete multiple variables at once
+# Get started
+## Installing
+At this moment you can install envfetch from Cargo:
+```shell
+cargo install envfetch
+```
+Also, you can install it from source
+```shell
+cargo install --git https://github.com/ankddev/envfetch envfetch
+```
+## Using
+To run envfetch, run `envfetch <COMMAND> <ARGS>` in your terminal.
+You can run `envfetch help` to see help message or `envfetch --version` to see program's version.
+### Command list
+#### Set
+Set environment variable and run process. Note that variable sets only for one run
+
+Usage:
+`envfetch set <KEY> <VALUE> <PROCESS>`, where:
+- `KEY` - name of environment variable
+- `VALUE` - value of environment variable
+- `PROCESS` - name of process which you want to run
+Options:
+- `--help`/`-h` - show help message
+
+For example:
+```shell
+envfetch set MY_VAR "Hello" "npm run"
+```
+It will set environment variable with name `MY_VAR` value "Hello" and start `npm run`
+
+#### Print
+Print all environment variables
+
+Usage:
+`envfetch print`
+Options:
+- `--help`/`-h` - show help message
+
+For example:
+```shell
+$ envfetch print
+SHELL = "powershell"
+windir = "C:\\Windows"
+SystemDrive = "C:"
+SystemRoot = "C:\\Windows"
+...
+```
+It will print all environment variables in format `VAR = "VALUE"`.
+#### Get
+Get value of environment variable
+
+Usage:
+`envfetch get <KEY>`, where:
+- `KEY` - name of environment variable
+Options:
+- `--help`/`-h` - show help message
+- `--no-similar-names`/`-s` - disable showing similar variables if variable not
+
+For example:
+```shell
+$ envfetch get MY_VAR
+"Hello"
+```
+It will print value of specified variable.
+#### Delete
+Delete variable and start process. Note that variable deletes only for one run
+
+Usage:
+`envfetch delete <KEY> <PROCESS>`, where:
+- `KEY` - name of environment variable
+- `PROCESS` - name of command to run
+  Options:
+- `--help`/`-h` - show help message
+
+For example:
+```shell
+envfetch delete MY_VAR "npm run"
+```
+It will delete variable `MY_VAR` and run `npm run` command.
+#### Load
+Load environment variables from dotenv-style file and run process. Note that variables sets only for one run
+
+Usage:
+`envfetch load <PROCESS>`, where:
+- `PROCESS` - name of process which you want to run
+  Options:
+- `--help`/`-h` - show help message
+- `--file <FILE>`/`-f <FILE>` - relative or absolute path to file to read variables from. Note that it must in .env format.
+  By default, program loads variables from `.env` file in current directory.
+
+For example:
+```shell
+envfetch load "npm run"
+envfetch load "npm run" --file ".env.debug"
+```
+It will load variables from `.env` or `.env.debug` and start `npm run`
+# Building from source
+- Install Rust. If it already installed, update with
+```shell
+$ rustup update
+```
+- Fork this project using button `Fork` on the top of this page
+- Clone your fork (replace `<YOUR_USERNAME>` with your username on GitHub):
+```shell
+$ git clone https://github.com/<YOUR_USERNAME>/envfetch.git
+```
+- Go to directory, where you cloned envfetch:
+```shell
+$ cd envfetch
+```
+- Run program using Cargo (replace `<COMMAND>` and `<ARGS>` to your command and args):
+```shell
+$ cargo run -- <COMMAND> <ARGS>
+```
+# Contributing
+- Read [section above to build envfetch from source](#building-from-source)
+- Create new branch
+- Made your changes
+- Test that everything works correctly
+- Format and lint code with
+```shell
+$ cargo fmt
+$ cargo clippy
+```
+- Run tests with
+```shell
+$ cargo test
+```
+- Push changes
+- Open pull request
---
+++ src/main.rs
@@ -0,0 +1,202 @@
+//! Lightweight cross-platform CLI tool for working with environment variables
+//!
+//! Easily get value of environment variable, get list of all variables,
+//! run processes with specific variable value, or delete specific variable
+//! to run process without it
+
+use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
+use std::{env, fs, process};
+use subprocess::Exec;
+
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    after_help = "Get more info at project's repo: https://github.com/ankddev/envfetch",
+    after_long_help = "Get more info at project's GitHub repo available at https://github.com/ankddev/envfetch",
+    arg_required_else_help = true
+)]
+#[command(
+    about = "envfetch - lightweight tool for working with environment variables",
+    long_about = "envfetch is a lightweight cross-platform CLI tool for working with environment variables"
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+/// All tool's commands
+#[derive(Subcommand)]
+enum Commands {
+    /// Prints value of environment variable
+    Get(GetArgs),
+    /// Set environment variable and run given process.
+    /// Note that the variable sets only for one run
+    Set(SetArgs),
+    /// Delete environment variable and run given process.
+    /// Note that the variable deletes only for one run
+    Delete(DeleteArgs),
+    /// Load environment variables from dotenv file
+    Load(LoadArgs),
+    /// Prints all environment variables
+    Print,
+}
+
+/// Args for get command
+#[derive(Args, Debug)]
+pub struct GetArgs {
+    /// Environment variable name
+    #[arg(required = true)]
+    key: String,
+    /// Disable showing similar variables' names if variable not found
+    #[arg(long, short = 's', default_value = "false")]
+    no_similar_names: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct LoadArgs {
+    /// Process to start
+    #[arg(required = true)]
+    process: String,
+    /// Relative or absolute path to file to read variables from.
+    /// Note that it must in .env format
+    #[arg(long, short, default_value = ".env")]
+    file: String,
+}
+
+/// Args for set command
+#[derive(Args, Debug)]
+pub struct SetArgs {
+    /// Environment variable name
+    #[arg(required = true)]
+    key: String,
+    /// Value for environment variable
+    #[arg(required = true)]
+    value: String,
+    /// Process to start
+    #[arg(required = true)]
+    process: String,
+}
+
+/// Args for delete command
+#[derive(Args, Debug)]
+pub struct DeleteArgs {
+    /// Environment variable name
+    #[arg(required = true)]
+    key: String,
+    /// Process to start
+    #[arg(required = true)]
+    process: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        // Get command handler
+        Commands::Get(opt) => {
+            // Check if variable with specified name exists
+            match env::var(&opt.key) {
+                Ok(value) => println!("{:?}", &value),
+                // If variable not found
+                _ => {
+                    error(format!("can't find '{}'", &opt.key).as_str());
+                    // Check if we need to search for similar environment variables
+                    if !opt.no_similar_names {
+                        // Check for similar variables, if user made a mistake
+                        let mut similar: bool = false;
+                        for (str, _) in env::vars().into_iter() {
+                            // We print names only if it's similarity with given string is greater than 60%
+                            if similar_string::compare_similarity(
+                                &opt.key.to_lowercase(),
+                                &str.to_lowercase(),
+                            ) > 0.6
+                            {
+                                // Print header f it is not printed yet
+                                if !similar {
+                                    eprintln!("Did you mean:");
+                                    similar = true;
+                                }
+                                eprintln!("  {}", &str);
+                            }
+                        }
+                    }
+                    process::exit(1)
+                }
+            }
+        }
+        // Print command handler
+        Commands::Print => {
+            // Print all environment variables
+            for (key, value) in env::vars().into_iter() {
+                println!("{} = {:?}", &key.blue(), &value);
+            }
+        }
+        // Load command handler
+        Commands::Load(opt) => {
+            // Try to read file
+            match fs::read_to_string(&opt.file) {
+                Ok(content) => {
+                    // Try to parse file
+                    match dotenv_parser::parse_dotenv(&content) {
+                        Ok(variables) => {
+                            for (key, value) in variables.into_iter() {
+                                unsafe { env::set_var(key, value) };
+                            }
+                            run(opt.process);
+                        }
+                        Err(err) => {
+                            error(err.to_string().as_str());
+                            run(opt.process);
+                            process::exit(1);
+                        }
+                    }
+                }
+                Err(err) => {
+                    error(err.to_string().as_str());
+                    run(opt.process);
+                    process::exit(1);
+                }
+            }
+        }
+        // Set command handler
+        Commands::Set(opt) => {
+            unsafe { env::set_var(opt.key, opt.value) };
+            run(opt.process);
+        }
+        // Delete command handler
+        Commands::Delete(opt) => {
+            // Check if variable exists
+            match env::var(&opt.key) {
+                Ok(_) => unsafe { env::remove_var(&opt.key) },
+                _ => warning("error doesn't exists"),
+            }
+            run(opt.process);
+        }
+    }
+}
+
+/// Runs given command using system shell
+fn run(process: String) {
+    let result = Exec::shell(process).join().unwrap_or_else(|_| {
+        error("can't' start process");
+        // Exit with non-zero exit code if we can't start process
+        process::exit(1);
+    });
+
+    // Exit with non-zero exit code if process did not successful
+    if !result.success() {
+        process::exit(1);
+    }
+}
+
+/// Print info about error
+fn error(text: &str) {
+    eprintln!("{} {}", "error:".red(), text);
+}
+
+/// Print info about warning
+fn warning(text: &str) {
+    eprintln!("{} {}", "warning:".yellow(), text);
+}
